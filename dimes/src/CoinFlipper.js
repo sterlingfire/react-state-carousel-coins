bf2737a8e7578c061fc269be36701c2f64b4dcdf
J. Sterling Bishop <jsterlingbishop@gmail.com>
2021-01-19 16:45:07 -0800

    implemented coinflipper

---
+++ dimes/src/CoinFlipper.js
@@ -0,0 +1,35 @@
+import Coin from "./Coin";
+import {useState} from 'react';
+
+/* CoinFlipper component
+ * props: none
+ * state: countHeads:int, countTails:int
+ * App -> CoinFlipper -> Coin
+ */
+function CoinFlipper(){
+  const [countHeads, setCountHeads] = useState(0);
+  const [countTails, setCountTails] = useState(0);
+  const [coinSide, setCoinSide] = useState("");
+
+  function flipCoin(){
+    if (Math.random() > 0.5) {
+      setCoinSide("Heads");
+      setCountHeads(countHeads+1);
+    }
+    else {
+      setCoinSide("Tails");
+      setCountTails(countTails+1);
+    }
+  }
+
+  return (
+    <div>
+      <h1>Flip A Coin!</h1>
+      <Coin side={coinSide}/>
+      <button onClick={flipCoin}>Flip Me!</button>
+      <p>Out of {countHeads+countTails} flips, there have been {countHeads} heads, and {countTails} tails. </p>
+    </div>
+  );
+}
+
+export default CoinFlipper;

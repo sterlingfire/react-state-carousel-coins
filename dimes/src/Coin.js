6bb7388c6cd3cb16f8b7e14f6cf6ca34b2244687
David Lee <davidlee980804@gmail.com>
2021-01-19 16:58:47 -0800

    Finished Coin Component, does not display in beginning

---
+++ dimes/src/Coin.js
@@ -0,0 +1,23 @@
+import "./Coin.css";
+
+const SIDE_TO_URL = {
+  Heads: "https://upload.wikimedia.org/wikipedia/commons/d/dd/2017-D_Roosevelt_dime_obverse_transparent.png",
+  Tails: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/2017-D_Roosevelt_dime_reverse_transparent.png/220px-2017-D_Roosevelt_dime_reverse_transparent.png"
+} 
+
+/** Renders Coin component
+ * 
+ * Prop:
+ * - Side: "Heads", "Tails" or "" which determines the image
+ *   - "" does not display the image
+ */
+
+function Coin({side}) {
+  let imgUrl = SIDE_TO_URL[side];
+
+  return (<div>
+    { side && <img src={imgUrl} className="Coin" alt={side}></img> }
+  </div>);
+}
+
+export default Coin;
